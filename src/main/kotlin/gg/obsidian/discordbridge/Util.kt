32d286ee3630a2b80f40f7634d69c046ca5141fa
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-02-21 09:58:20 -0600

    Do not translate colors from Discord

--- src/main/kotlin/gg/obsidian/discordbridge/Util.kt
+++ src/main/kotlin/gg/obsidian/discordbridge/Util.kt
@@ -5,11 +5,12 @@ import org.bukkit.ChatColor
 object Util {
     fun formatMessage(message: String, replacements: Map<String, String>, colors: Boolean = false): String {
         var formattedString = message
+
+        if (colors) formattedString = ChatColor.translateAlternateColorCodes('&', formattedString)
+
         for ((token, replacement) in replacements) {
             formattedString = formattedString.replace(token, replacement)
         }
-
-        if (colors) formattedString = ChatColor.translateAlternateColorCodes('&', formattedString)
 
         return formattedString
     }
