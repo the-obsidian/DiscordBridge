7a20a36d5b3db1d9098129cef544bee8b7e01e79
DiamondIceNS <fawfulcopter@gmail.com>
2017-02-12 12:59:53 -0600

    Re-added the Util class after nuking it from orbit because I'm a dumbass

--- src/main/kotlin/gg/obsidian/discordbridge/Util.kt
+++ src/main/kotlin/gg/obsidian/discordbridge/Util.kt
@@ -1,17 +1,38 @@
 package gg.obsidian.discordbridge
 
 import org.bukkit.ChatColor
+import java.io.IOException
+import com.michaelwflaherty.cleverbotapi.CleverBotQuery
 
 object Util {
+
     fun formatMessage(message: String, replacements: Map<String, String>, colors: Boolean = false): String {
         var formattedString = message
-
-        if (colors) formattedString = ChatColor.translateAlternateColorCodes('&', formattedString)
 
         for ((token, replacement) in replacements) {
             formattedString = formattedString.replace(token, replacement)
         }
 
+        if (colors) formattedString = ChatColor.translateAlternateColorCodes('&', formattedString)
+
         return formattedString
+    }
+
+    fun askCleverbot(key: String, message: String): String {
+
+        if (key.isEmpty()) return "You do not have an API key. Go to https://www.cleverbot.com/api/ for more information."
+        val bot: CleverBotQuery = CleverBotQuery(key, message)
+        var response: String
+        try
+        {
+            bot.sendRequest()
+            response = bot.response
+        }
+        catch (e: IOException)
+        {
+            response = e.message!!
+        }
+
+        return response
     }
 }
